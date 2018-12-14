eef70640f9b28273a9f06b4cd6f2f96321d56c87
unknown <mattiacutrupi1996@gmail.com>
2018-12-14 15:46:04 +0100

    add image to login activiy and add list to access activity

--- app/src/main/java/com/example/pc/myapplication/AccessActivity.java
+++ app/src/main/java/com/example/pc/myapplication/AccessActivity.java
@@ -5,6 +5,8 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 public class AccessActivity extends AppCompatActivity implements View.OnClickListener {
@@ -12,6 +14,35 @@ public class AccessActivity extends AppCompatActivity implements View.OnClickLis
     TextView emailWL;
     String email;
     String openedEmail;
+
+    LinearLayout linearLL;
+
+    TextView textvwM;
+    TextView priceM;
+    TextView quantityM;
+
+    TextView textvwB;
+    TextView priceB;
+    TextView quantityB;
+
+    TextView textvwT;
+    TextView priceT;
+    TextView quanityT;
+
+    TextView totals;
+
+    Button minusM;
+    Button plusM;
+
+    Button minusB;
+    Button plusB;
+
+    Button minusT;
+    Button plusT;
+
+    Button buyY;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,6 +64,35 @@ public class AccessActivity extends AppCompatActivity implements View.OnClickLis
             openedEmail=Uri.decode(openedEmail);
             emailWL.setText(openedEmail);
         }
+
+        linearLL = findViewById(R.id.linearL);
+
+        textvwM = findViewById(R.id.textVW);
+        priceM = findViewById(R.id.prize);
+        quantityM = findViewById(R.id.qnt);
+
+        textvwB = findViewById(R.id.textVW2);
+        priceB = findViewById(R.id.prize2);
+        quantityB = findViewById(R.id.qnt2);
+
+        textvwT = findViewById(R.id.textVW3);
+        priceT = findViewById(R.id.prize3);
+        quanityT = findViewById(R.id.qnt3);
+
+        totals = findViewById(R.id.tot);
+
+        minusM = findViewById(R.id.minus);
+        plusM = findViewById(R.id.plus);
+
+        minusB = findViewById(R.id.minus2);
+        plusB = findViewById(R.id.plus2);
+
+        minusT = findViewById(R.id.minus3);
+        plusT = findViewById(R.id.plus3);
+
+        buyY = findViewById(R.id.buy);
+
+        linearLL.setBackgroundColor(getResources().getColor(R.color.colorBGAA));
 
     }
 
